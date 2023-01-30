5685cb1984f2a1ca3ce58e77fbe5cd60bd5c620a
aamrlyman <aamrlyman@gmail.com>
2023-01-29 20:37:42 -0800

    add edit, delete, and get functions to ingredient

--- backend/meal_ingredients/urls.py
+++ backend/meal_ingredients/urls.py
@@ -6,5 +6,6 @@ from meal_ingredients import views
 urlpatterns = [
     path('', views.get_ingredients_by_meal_id),
     path('add/', views.add_ingredient_to_meal),
+    path('<int:pk>/', views.ingredient_detail),
     # path('all/', views.get_all_cars),
 ]
--- backend/meal_ingredients/views.py
+++ backend/meal_ingredients/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Meal_Ingredient
 from .serializers import Meal_IngredientSerializer
+from django.shortcuts import get_object_or_404
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -16,7 +18,7 @@ def get_ingredients_by_meal_id(request):
     return Response(serializer.data)
 
 
-@api_view(['GET', 'PUT', 'POST'])
+@api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def add_ingredient_to_meal(request):
     print(
@@ -27,6 +29,25 @@ def add_ingredient_to_meal(request):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "DELETE"])
+@permission_classes([IsAuthenticated])
+def ingredient_detail(request, pk):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    ingredient = get_object_or_404(Meal_Ingredient, pk=pk)
+    if request.method == 'GET':
+        serializer = Meal_IngredientSerializer(ingredient);
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = Meal_IngredientSerializer(ingredient, data=request.data);
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+             ingredient.delete()
+             return Response(status = status.HTTP_204_NO_CONTENT)
+
 
     # elif request.method == 'GET':
     #     cars = Car.objects.filter(user_id=request.user.id)
