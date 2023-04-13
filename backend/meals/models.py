01de352d9c286836c627c13ff8c425e523922ff1
aamrlyman <aamrlyman@gmail.com>
2023-04-13 08:14:13 -0700

    update max-character length for meal name and meal notes

--- backend/meals/models.py
+++ backend/meals/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-from django.db import models
 from authentication.models import User
 
 # Create your models here.
@@ -10,8 +8,8 @@ from authentication.models import User
 
 class Meal(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    notes = models.CharField(max_length=500, blank=True, default= '')
+    name = models.CharField(max_length=60)
+    notes = models.CharField(max_length=1000, blank=True, default= '')
     url = models.CharField(max_length=150, blank=True, default= '')
     prep_time_minutes = models.IntegerField(blank=True, default = 0)
     prep_time_hours = models.IntegerField(blank=True, default = 0)
